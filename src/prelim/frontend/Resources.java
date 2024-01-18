3bd1adc1f1953d562d2343ff1107c9bb288ff649
Johan Rickardo Roxas <75343741+rickardo-roxas@users.noreply.github.com>
2024-01-18 21:33:34 +0800

    Added color objects in Resources

---
+++ src/prelim/frontend/Resources.java
@@ -0,0 +1,11 @@
+package prelim.frontend;
+
+import java.awt.*;
+
+public class Resources {
+    public final Color imperialRed = new Color(241,54,64);
+    public final Color davysGray = new Color(96,95,94);
+    public final Color delftBlue = new Color(29,52,97);
+    public final Color polynesianBlue = new Color(31,72,126);
+    public final Color cerulean = new Color(36,123,160);
+}
