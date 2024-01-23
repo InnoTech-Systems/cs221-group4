ff85b0c822cfa434a5046f2a621900bfc51719ac
Johan Rickardo Roxas <75343741+rickardo-roxas@users.noreply.github.com>
2024-01-23 21:16:33 +0800

    Initial structure for Controller class

--- src/prelim/controller/Controller.java
+++ src/prelim/controller/Controller.java
@@ -1,4 +1,14 @@
 package prelim.controller;
 
-public class UIController {
+import prelim.model.Athlete;
+import prelim.view.GUI;
+
+public class Controller {
+    private DataHandler dataHandler;
+    private GUI view;
+
+    public Controller(DataHandler dataHandler, GUI view) {
+        this.dataHandler = dataHandler;
+        this.view = view;
+    }
 }
