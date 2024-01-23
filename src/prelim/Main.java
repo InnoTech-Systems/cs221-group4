d96f7c8d9152c01009dcdbbd0a2c3631abe05635
Johan Rickardo Roxas <75343741+rickardo-roxas@users.noreply.github.com>
2024-01-23 22:01:17 +0800

    Added controller instantiation in Main

--- src/prelim/Main.java
+++ src/prelim/Main.java
@@ -1,16 +1,23 @@
 package prelim;
 
+import prelim.controller.Controller;
+import prelim.controller.DataHandler;
 import prelim.view.GUI;
 
 import javax.swing.*;
+import javax.xml.crypto.Data;
 
 public class Main {
     public static void main(String[] args) {
+
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 final GUI gui = new GUI();
                 gui.setVisible(true);
+
+                DataHandler dh = new DataHandler();
+                Controller controller = new Controller(dh, gui);
             }
         });
     }
