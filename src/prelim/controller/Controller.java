985a7c49c764fb8d5ec60554245a160957b5c941
Johan Rickardo Roxas <75343741+rickardo-roxas@users.noreply.github.com>
2024-01-23 22:02:39 +0800

    Added todo list in the controller and an olympics logo image for the home page.

--- src/prelim/controller/Controller.java
+++ src/prelim/controller/Controller.java
@@ -10,5 +10,9 @@ public class Controller {
     public Controller(DataHandler dataHandler, GUI view) {
         this.dataHandler = dataHandler;
         this.view = view;
+
+        // Button Implementation
+        // Create method to map filterOptions to an integer
+        // Create a switch-case block to update the model
     }
 }
